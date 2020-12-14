4f33d36c9edd6815449ecde2e99318d733adedd1
alex-rak <2304alex2304@gmail.com>
2020-12-14 14:24:32 +0500

    add draft components

---
+++ src/components/Wallets/HeaderWallet.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-toolbar class="header-wallet" color="purple">
+    <v-toolbar-title class="header-wallet__title">Bitcoin (Personal)</v-toolbar-title>
+
+    <v-spacer />
+
+    <v-btn icon color="white">
+      <v-icon>mdi-share-variant</v-icon>
+    </v-btn>
+
+    <v-btn icon color="white">
+      <v-icon>mdi-dots-vertical</v-icon>
+    </v-btn>
+  </v-toolbar>
+</template>
+
+<script>
+export default {
+  name: 'HeaderWallet'
+}
+</script>
+
+<style lang="scss">
+.header-wallet {
+  &__title {
+    color: $--white;
+  }
+}
+</style>
---
+++ src/components/Wallets/ItemTransaction.vue
@@ -0,0 +1,55 @@
+<template>
+  <v-list-item class="item-transition">
+    <v-list-item-avatar size="60">
+      <v-icon
+        class="item-transition__icon grey lighten-1"
+        dark
+        x-large
+        :class="{
+          'item-transition__icon--received': type.toLowerCase() === 'received'
+        }"
+      >
+        mdi-arrow-bottom-left
+      </v-icon>
+    </v-list-item-avatar>
+    <template>
+      <v-list-item-content class="text-left">
+        <v-list-item-title>
+          <span>{{ type }}</span>
+          <span>{{ status }}</span>
+        </v-list-item-title>
+        <v-list-item-subtitle>
+          <span>{{ time }}</span>
+        </v-list-item-subtitle>
+      </v-list-item-content>
+      <v-list-item-action>
+        <v-list-item-title>
+          <span>{{ currency.toUpperCase() }}</span>
+          <span>{{ value }}</span>
+        </v-list-item-title>
+        <v-list-item-subtitle>
+          <span>USD</span>
+          <span>{{ valueInUsd }}</span>
+        </v-list-item-subtitle>
+      </v-list-item-action>
+    </template>
+  </v-list-item>
+</template>
+
+<script>
+export default {
+  name: 'ItemTransaction',
+  props: {
+    currency: {
+      type: String,
+      default: ''
+    },
+    type: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
+
+<style lang="scss"></style>
