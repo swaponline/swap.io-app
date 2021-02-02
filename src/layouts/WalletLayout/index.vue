f809bc40aca88cbb2b8cceb569f50e8ba3e42e45
alex-rak <2304alex2304@gmail.com>
2021-02-02 12:07:51 +0500

    fix: desktop-animation wallet

--- src/layouts/WalletLayout/index.vue
+++ src/layouts/WalletLayout/index.vue
@@ -6,7 +6,7 @@
     ></list-wallet>
     <div class="wallet-layout__router" :class="showListWallet ? 'wallet-layout__router--hide' : ''">
       <transition-translate :reverse="metaBack">
-        <router-view :key="currentRoute"></router-view>
+        <router-view :key="currentRoute" class="wallet-layout__router-content"></router-view>
       </transition-translate>
     </div>
     <main-actions></main-actions>
@@ -23,6 +23,7 @@ import AllModals from '@/components/Wallets/AllModals.vue'
 
 export default {
   name: 'WalletLayout',
+  inject: ['mediaQueries'],
   components: {
     TransitionTranslate,
     ListWallet,
@@ -31,7 +32,10 @@ export default {
   },
   computed: {
     currentRoute() {
-      return this.$route.name + JSON.stringify(this.$route.params) + JSON.stringify(this.$route.query)
+      if (this.mediaQueries.desktop) {
+        return this.$route.name
+      }
+      return this.$route.name + JSON.stringify(this.$route.params)
     },
     walletParam() {
       return this.$route.params.walletAddress
@@ -89,6 +93,14 @@ export default {
       &--hide {
         transform: translateX(100vw);
       }
+    }
+  }
+  &__router-content {
+    position: relative;
+    @include tablet {
+      // убираем анимацию оставляем только transition
+      transform: translateX(0) !important;
+      opacity: 1 !important;
     }
   }
 }
