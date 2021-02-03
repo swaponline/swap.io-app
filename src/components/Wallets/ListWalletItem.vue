5b894aa7f3017f32ec74d3612d57c90cd6d4f288
alex-rak <2304alex2304@gmail.com>
2021-02-03 15:55:58 +0500

    fix: list wallet components

---
+++ src/components/Wallets/ListWalletItem.vue
@@ -0,0 +1,85 @@
+<template>
+  <v-list-item
+    link
+    exact
+    class="list-wallet-item"
+    :to="{ name: 'Wallet', params: { walletAddress: subWallets[0].address } }"
+  >
+    <v-list-item-icon class="list-wallet-item__icon-wrapper">
+      <svg-icon class="list-wallet-item__icon" name="btc" />
+    </v-list-item-icon>
+    <v-list-item-content>
+      <v-list-item-title class="list-wallet-item__title">
+        <span>{{ nameCurrency }}</span>
+        <span>{{ value }}</span>
+        <span class="list-wallet-item__name">{{ subWallets[0].name }}</span>
+      </v-list-item-title>
+    </v-list-item-content>
+  </v-list-item>
+</template>
+
+<script>
+export default {
+  name: 'ListWalletItem',
+  props: {
+    name: {
+      type: String,
+      default: ''
+    },
+    nameCurrency: {
+      type: String,
+      default: ''
+    },
+    value: {
+      type: Number,
+      default: 0
+    },
+    subWallets: {
+      type: Array,
+      default: () => []
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.list-wallet-item {
+  min-height: 40px;
+  border-radius: 12px;
+  margin: 5px 10px;
+  padding: 0 15px;
+  overflow: hidden;
+  &:first-child {
+    margin-top: 5px;
+  }
+  &:before {
+    z-index: 0;
+    background: $--black;
+  }
+  &__icon-wrapper {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border-radius: 50%;
+    min-width: 45px;
+    height: 45px;
+    margin: 15px 16px 15px 0 !important;
+  }
+  &__icon {
+    width: 45px;
+    height: 45px;
+    fill: $--white;
+  }
+  &__title {
+    display: flex;
+    justify-content: space-between;
+    flex-wrap: wrap;
+    line-height: 25px !important;
+  }
+  &__name {
+    width: 100%;
+    font-size: $--font-size-extra-small-subtitle;
+    color: $--grey;
+  }
+}
+</style>
