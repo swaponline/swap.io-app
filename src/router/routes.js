81c6c622c58f5e7b8c582ccee8c1006e2076c26b
alex-rak <2304alex2304@gmail.com>
2021-02-24 14:01:42 +0500

    fix routes and layouts

--- src/router/routes.js
+++ src/router/routes.js
@@ -2,7 +2,7 @@ export default [
   {
     path: '/',
     name: 'MainLayout',
-    component: () => import(/* webpackChunkName: 'MainLayout' */ '@/layouts/LayoutRedesign'),
+    component: () => import(/* webpackChunkName: 'MainLayout' */ '@/layouts/MainLayout'),
     redirect: { name: 'Wallet' },
     children: [
       {
@@ -18,20 +18,43 @@ export default [
           }
         ]
       },
-      {
-        path: '/swap',
-        name: 'Swap',
-        component: () => import(/* webpackChunkName: 'SecretPhrase' */ '@/views/Profile/SecurityInfo.vue')
-      },
+      // {
+      //   path: '/swap',
+      //   name: 'Swap',
+      //   component: () => import(/* webpackChunkName: 'Swap' */ '@/views/Swap')
+      // },
       {
         path: '/history',
         name: 'History',
-        component: () => import(/* webpackChunkName: 'Wallet' */ '@/views/Profile/SecretPhrase.vue')
+        component: () => import(/* webpackChunkName: 'Wallet' */ '@/views/Wallets/Wallet.vue')
       },
       {
         path: '/settings',
         name: 'Settings',
         component: () => import(/* webpackChunkName: 'Wallet' */ '@/views/Wallets/Wallet.vue')
+      }
+    ]
+  },
+  {
+    path: '/',
+    name: 'ProfileLayout',
+    component: () => import(/* webpackChunkName: 'ProfileLayout' */ '@/layouts/ProfileLayout'),
+    redirect: { name: 'MainLayout' },
+    children: [
+      {
+        path: '/security-info',
+        name: 'SecurityInfo',
+        component: () => import(/* webpackChunkName: 'SecurityInfo' */ '@/views/Profile/SecurityInfo.vue')
+      },
+      {
+        path: '/secret-phrase',
+        name: 'SecretPhrase',
+        component: () => import(/* webpackChunkName: 'SecretPhrase' */ '@/views/Profile/SecretPhrase.vue')
+      },
+      {
+        path: '/choose-style',
+        name: 'ChooseStyle',
+        component: () => import(/* webpackChunkName: 'ChooseStyle' */ '@/views/Profile/ChooseStyle.vue')
       }
     ]
   }
