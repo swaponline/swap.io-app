bca273f0dc935ea9816e2056b32866db94534017
alex-rak <2304alex2304@gmail.com>
2021-03-03 12:01:43 +0500

    fix: generateColor

--- src/utils/generators.js
+++ src/utils/generators.js
@@ -1,6 +1,16 @@
+export const shuffle = array => {
+  for (let i = array.length - 1; i > 0; i -= 1) {
+    const j = Math.floor(Math.random() * (i + 1))
+    // eslint-disable-next-line no-param-reassign
+    ;[array[i], array[j]] = [array[j], array[i]]
+  }
+}
 export const generateColor = () => {
-  const color = Math.floor(Math.random() * 16777216).toString(16)
-  return '#000000'.slice(0, -color.length) + color
+  const colorsDefault = ['ff', '00']
+  const color = Math.floor(Math.random() * 255).toString(16)
+  colorsDefault.push(color)
+  shuffle(colorsDefault)
+  return '#000000'.slice(0, -colorsDefault.join('').length) + colorsDefault.join('')
 }
 export const randomInteger = (min, max) => {
   const rand = min + Math.random() * (max + 1 - min)
