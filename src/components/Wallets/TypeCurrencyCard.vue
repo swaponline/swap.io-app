0e399113cbc28a90e480d08d0733d5214b89abc4
alex-rak <2304alex2304@gmail.com>
2021-03-11 17:09:24 +0500

    radioInput: visually-hidden

--- src/components/Wallets/TypeCurrencyCard.vue
+++ src/components/Wallets/TypeCurrencyCard.vue
@@ -2,7 +2,7 @@
   <label class="type-currency-card" :class="{ 'type-currency-card--selected': selected }" type="button">
     <svg-icon class="type-currency-card__icon" :name="`cards/card-${iconName}`"></svg-icon>
     <span class="type-currency-card__name">{{ name }}</span>
-    <input style="display: none" type="radio" name="currency" @input="$emit('select')" />
+    <input class="type-currency-card__input" tabindex="0" type="radio" name="currency" @input="$emit('select')" />
   </label>
 </template>
 
@@ -28,6 +28,7 @@ export default {
 
 <style lang="scss">
 .type-currency-card {
+  position: relative;
   display: flex;
   flex-direction: column;
   border: 1px solid $--black;
@@ -35,6 +36,7 @@ export default {
   min-height: 107px;
   padding: 10px 10px;
   outline: none;
+
   @include tablet {
     min-height: 112px;
   }
@@ -42,6 +44,10 @@ export default {
     min-height: 100px;
   }
   &--selected {
+    padding: 9px 9px;
+    border: 2px solid $--purple;
+  }
+  &:focus-within {
     padding: 9px 9px;
     border: 2px solid $--purple;
   }
@@ -53,6 +59,13 @@ export default {
     font-size: $--font-size-medium;
     line-height: 22px;
     margin-top: 11px;
+  }
+  &__input {
+    position: absolute;
+    clip: rect(0 0 0 0);
+    width: 1px;
+    height: 1px;
+    margin: -1px;
   }
 }
 </style>
