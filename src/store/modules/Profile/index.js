64e57ccdf5ff230cc54b1376d03bacc263ed0643
alex-rak <2304alex2304@gmail.com>
2021-04-16 16:09:13 +0500

    save profile in storage

--- src/store/modules/Profile/index.js
+++ src/store/modules/Profile/index.js
@@ -1,10 +1,7 @@
-import axios from 'axios'
-import { randomInteger, generateColor, getGradient } from '@/utils/generators'
-import { SET_LIST, UPDATE_MODEL } from '../../common/mutations.types'
+import { UPDATE_MODEL } from '../../common/mutations.types'
 
 export const MODULE_NAME = 'Profile'
 export const SET_USERS_COLORS = 'SET_USERS_COLORS'
-export const FILL_CARDS = 'FILLCARDS'
 
 export default {
   state: {
@@ -18,30 +15,6 @@ export default {
   actions: {
     [SET_USERS_COLORS]({ commit }, params) {
       commit(UPDATE_MODEL, { name: MODULE_NAME, model: { ...params } })
-    },
-    [FILL_CARDS]({ commit }) {
-      return axios
-        .get('https://raw.githubusercontent.com/bitcoin/bips/master/bip-0039/english.txt')
-        .then(({ data }) => {
-          const words = data.split('\n').slice(0, -1)
-          const list = []
-          for (let i = 0; i < 4; i += 1) {
-            const wordList = []
-            for (let k = 0; k < 24; k += 1) {
-              const word = words.splice(randomInteger(0, words.length - 1), 1)
-              wordList.push(...word)
-            }
-
-            const color = generateColor()
-            list.splice(i, 1, {
-              background: getGradient(color),
-              color,
-              wordList
-            })
-          }
-          commit(SET_LIST, { name: MODULE_NAME, list })
-          return Promise.resolve()
-        })
     }
   }
 }
--- src/views/CreateProfile/index.vue
+++ src/views/CreateProfile/index.vue
@@ -6,6 +6,9 @@
 
 <script>
 import WindowHandler from '@/WindowHandler'
+import { mapActions } from 'vuex'
+import { SET_USERS_COLORS } from '@/store/modules/Profile'
+import { getStorage, setStorage } from '@/utils/storage'
 
 export default {
   name: 'CreateProfile',
@@ -18,13 +21,23 @@ export default {
     this.openFrame()
   },
   methods: {
+    ...mapActions({
+      actionSetBackground: SET_USERS_COLORS
+    }),
     async openFrame() {
       const { newProfile } = await new Promise(resolve => {
         this.frame = new WindowHandler('createProfile', '/choose-style', 'CreateProfile', e => {
           resolve(e)
         })
       })
+      this.actionSetBackground({
+        background: newProfile.background,
+        color: newProfile.color
+      })
       console.log(newProfile)
+      const profiles = JSON.parse(getStorage('profiles')) || {}
+      profiles[newProfile.publicKey.slice(0, 10)] = newProfile
+      setStorage('profiles', JSON.stringify(profiles))
     }
   }
 }
