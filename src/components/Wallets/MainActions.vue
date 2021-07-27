3bdb2e3db917080102f5d59b571304a19fd20eae
Nikita Getman <nikita.getman56@gmail.com>
2021-07-27 10:53:47 +0700

    button dragging

--- src/components/Wallets/MainActions.vue
+++ src/components/Wallets/MainActions.vue
@@ -1,127 +1,185 @@
 <template>
-  <v-speed-dial
-    v-model="isOpen"
-    class="main-actions"
-    bottom
-    left
-    direction="top"
-    transition="slide-y-reverse-transition"
-  >
-    <template #activator>
-      <v-btn v-model="isOpen" class="main-actions__open-button" dark fab height="auto" width="auto">
-        <v-icon class="main-actions__open-icon" :class="{ 'main-actions__open-icon--rotated': isOpen }">
-          mdi-close
-        </v-icon>
-      </v-btn>
-    </template>
-    <v-tooltip right>
-      <template #activator="{ on, attrs }">
-        <v-btn
-          fab
-          dark
-          color="green"
-          class="main-actions__button"
-          height="auto"
-          width="auto"
-          v-bind="attrs"
-          @click="openWalletCreate"
-          v-on="on"
+  <match-media v-slot="{ desktop }" class="main-actions" tag="div">
+    <v-fade-transition>
+      <div v-if="isDragging" class="main-actions__background">
+        <p class="main-actions__text">You can move the menu button to any of the available locations</p>
+      </div>
+    </v-fade-transition>
+
+    <div class="main-actions__layout">
+      <template v-if="desktop">
+        <div
+          v-for="(spot, index) in $options.SPOTS"
+          :key="index"
+          ref="spot"
+          class="main-actions__spot"
+          :class="{ 'main-actions__spot--hidden': !isDragging }"
+          :style="spot"
         >
-          <v-icon>mdi-wallet</v-icon>
-        </v-btn>
+          <swap-icon-menu-spot class="main-actions__spot-icon" />
+        </div>
       </template>
-      <span>New address</span>
-    </v-tooltip>
-    <v-tooltip right>
-      <template #activator="{ on, attrs }">
-        <v-btn
-          fab
-          dark
-          :to="{ name: 'CreateOrRecover' }"
-          color="blue"
-          class="main-actions__button"
-          height="auto"
-          width="auto"
-          v-bind="attrs"
-          v-on="on"
-        >
-          <v-icon>mdi-account-plus</v-icon>
-        </v-btn>
-      </template>
-      <span>New profile</span>
-    </v-tooltip>
-    <v-tooltip right>
-      <template #activator="{ on, attrs }">
-        <v-btn
-          fab
-          dark
-          color="indigo"
-          class="main-actions__button main-actions__button--rotate"
-          height="auto"
-          width="auto"
-          v-bind="attrs"
-          v-on="on"
-          @click="openInvoiceBlock"
-        >
-          <v-icon>mdi-arrow-down-circle</v-icon>
-        </v-btn>
-      </template>
-      <span>Receive</span>
-    </v-tooltip>
-    <v-tooltip right>
-      <template #activator="{ on, attrs }">
-        <v-btn
-          fab
-          dark
-          color="indigo"
-          class="main-actions__button main-actions__button--rotate"
-          height="auto"
-          width="auto"
-          v-bind="attrs"
-          v-on="on"
-          @click="openSendForm"
-        >
-          <v-icon>mdi-arrow-up-circle</v-icon>
-        </v-btn>
-      </template>
-      <span>Send</span>
-    </v-tooltip>
-  </v-speed-dial>
+
+      <main-actions-button
+        ref="menuButton"
+        class="main-actions__button"
+        :class="{ 'main-actions__button--dragging': isDragging }"
+        :direction="openDirection"
+        :draggable="desktop"
+        @dragstart="dragStart"
+        @mousedown="start"
+        @mouseup="end"
+        @click="click"
+      />
+    </div>
+  </match-media>
 </template>
 
 <script>
-import { INVOICE_FORM, SEND_FORM, WALLET_CREATE } from '@/store/modules/Modals/names'
-import { ADD_MODAL } from '@/store/modules/Modals'
-import { mapMutations } from 'vuex'
+import SwapIconMenuSpot from '@/components/UI/Icons/MenuSpot.vue'
+import MainActionsButton from '@/components/Wallets/MainActionsButton.vue'
+import { MatchMedia } from 'vue-component-media-queries'
+import { getCoords, getCenter, getDistance } from '@/utils/common'
+import { getStorage, setStorage } from '@/utils/storage'
+
+const CURRENT_SPOT_KEY = 'buttonSpot'
+const SPOT_ACTIVATION_DISTANCE = 300
+
+const spots = [
+  { left: '8px', bottom: '52%' },
+  { left: '8px', bottom: '10px' },
+  { left: '22%', bottom: '10px' },
+  { left: '78%', bottom: '10px' },
+  { left: 'calc(100% - 86px)', bottom: '10px' },
+  { left: 'calc(100% - 86px)', bottom: '52%' }
+]
 
 export default {
   name: 'MainActions',
+  components: { SwapIconMenuSpot, MainActionsButton, MatchMedia },
+  SPOTS: spots,
+  inject: ['mediaQueries'],
   data() {
     return {
-      isOpen: false
+      isDragging: false,
+      isMoved: false,
+      currentSpot: 1,
+      offset: {},
+      activeSpot: null,
+      clickDebounce: null,
+      isMenuOpen: false,
+      openDirection: 'top'
     }
   },
+  watch: {
+    'mediaQueries.desktop': {
+      handler(val) {
+        if (val) {
+          this.$nextTick(() => {
+            this.setButtonPosition(this.currentSpot)
+          })
+        } else {
+          // reset button position
+          const button = this.$refs.menuButton.$el
+          button.style = ``
+        }
+      }
+    }
+  },
+  mounted() {
+    this.currentSpot = getStorage(CURRENT_SPOT_KEY) || 1
+    window.addEventListener('resize', this.handleResize)
+    this.setButtonPosition(this.currentSpot)
+  },
+  beforeDestroy() {
+    window.removeEventListener('resize', this.handleResize)
+  },
   methods: {
-    ...mapMutations({
-      mutationAddModal: ADD_MODAL
-    }),
-    openInvoiceBlock() {
-      this.mutationAddModal({
-        name: INVOICE_FORM,
-        show: true
-      })
+    dragStart(e) {
+      this.isMoved = true
+      e.preventDefault()
+      return false
     },
-    openSendForm() {
-      this.mutationAddModal({
-        name: SEND_FORM,
-        show: true
-      })
+    handleResize() {
+      this.setButtonPosition(this.currentSpot)
     },
-    openWalletCreate() {
-      this.mutationAddModal({
-        name: WALLET_CREATE,
-        show: true
-      })
+    start(e) {
+      this.clickDebounce = setTimeout(() => {
+        const button = this.$refs.menuButton.$el
+
+        const coords = getCoords(button)
+        this.offset = { x: e.pageX - coords.left, y: e.pageY - coords.top }
+        this.move(e)
+
+        this.isDragging = true
+        document.onmousemove = this.move
+      }, 200)
+    },
+    move(e) {
+      const button = this.$refs.menuButton.$el
+      button.style = `left: ${e.pageX - this.offset.x}px; top: ${e.pageY - this.offset.y}px;`
+
+      const mousePosition = { x: e.pageX, y: e.pageY }
+      if (this.$refs.spot) {
+        const spotDistances = this.$refs.spot.map((el, index) => {
+          const coords = getCenter(el)
+          return { el, index, distance: getDistance(mousePosition, { x: coords.left, y: coords.top }) }
+        })
+
+        spotDistances.sort((a, b) => a.distance - b.distance)
+
+        const [nearestSpot] = spotDistances
+
+        if (nearestSpot && nearestSpot.distance < SPOT_ACTIVATION_DISTANCE) {
+          if (nearestSpot.el !== this.activeSpot?.el) {
+            if (this.activeSpot) {
+              this.activeSpot.el.classList.remove('active')
+            }
+            this.activeSpot = nearestSpot
+            this.activeSpot.el.classList.add('active')
+          }
+        } else if (this.activeSpot) {
+          this.activeSpot.el.classList.remove('active')
+          this.activeSpot = null
+        }
+      }
+    },
+    click(e) {
+      if (this.isMoved) {
+        e.stopPropagation()
+        this.isMoved = false
+      }
+    },
+    end() {
+      clearTimeout(this.clickDebounce)
+      this.clickDebounce = null
+
+      if (this.isDragging) {
+        if (this.activeSpot) {
+          this.currentSpot = this.activeSpot.index
+        }
+        this.setButtonPosition(this.currentSpot)
+
+        this.openDirection = 'top'
+        if (this.currentSpot === 0) this.openDirection = 'right'
+        if (this.currentSpot === 5) this.openDirection = 'left'
+
+        this.isDragging = false
+        this.activeSpot = null
+        document.onmousemove = null
+      }
+    },
+    setButtonPosition(spotIndex) {
+      const button = this.$refs.menuButton.$el
+
+      if (this.mediaQueries.desktop) {
+        const coords = getCoords(this.$refs.spot[spotIndex])
+        button.style = `left: ${coords.left}px; top: ${coords.top}px;`
+
+        setStorage(CURRENT_SPOT_KEY, spotIndex)
+      } else {
+        button.style = ``
+      }
     }
   }
 }
@@ -129,77 +187,93 @@ export default {
 
 <style lang="scss">
 .main-actions {
-  &__open-button {
-    background: var(--main-color) !important;
-    min-width: 65px;
-    min-height: 65px;
+  $this: &;
 
-    @include tablet {
-      min-width: 75px;
-      min-height: 75px;
-    }
+  &__background {
+    position: fixed;
+    left: 0;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    z-index: 1000;
+    background-color: rgba($--black, 0.5);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+  }
+  &__text {
+    max-width: 620px;
+    font-size: $--font-size-extra-title;
+    line-height: 49px;
+    font-weight: $--font-weight-bold;
+    color: $--white;
+  }
 
-    @include phone {
-      min-width: 50px;
-      min-height: 50px;
+  &__layout {
+    position: absolute;
+    left: 0;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    z-index: 1010;
+    pointer-events: none;
+
+    &--active {
+      pointer-events: all;
     }
   }
 
-  &__open-icon {
-    font-size: 32px !important;
-    transform: rotate(135deg);
-    @include tablet {
-      font-size: 40px !important;
-    }
-    @include phone {
-      font-size: 26px !important;
+  &__spot {
+    position: absolute;
+    width: 78px;
+    height: 78px;
+    transition: all 0.3s;
+    border-radius: 50%;
+
+    &--hidden {
+      opacity: 0;
     }
 
-    &--rotated {
-      transform: rotate(0);
+    &.active {
+      #{$this}__spot-icon {
+        animation: active-rotation 2s linear infinite;
+        background-color: rgba($--white, 0.4);
+        box-shadow: 0 0 20px rgba($--white, 0.6);
+      }
     }
+  }
+  &__spot-icon {
+    border-radius: 50%;
+    animation: default-rotation 2.5s ease-in-out infinite alternate;
+    transition: all 0.3s;
+    transform-origin: center;
   }
 
   &__button {
-    min-width: 55px;
-    min-height: 55px;
+    pointer-events: all;
+    transition: all 0.3s;
+    position: fixed;
 
-    @include tablet {
-      min-width: 45px;
-      min-height: 45px;
+    &--dragging {
+      transition: none;
     }
+  }
 
-    @include phone {
-      min-width: 40px;
-      min-height: 40px;
+  @keyframes default-rotation {
+    0% {
+      transform: rotate(-45deg);
     }
-
-    &--rotate {
+    100% {
       transform: rotate(45deg);
     }
   }
-}
-
-.v-speed-dial {
-  position: absolute;
-
-  @include tablet {
-    position: fixed;
-    bottom: 80px !important;
-    z-index: 100;
-    transition: 0.5s;
-
-    &--wallet {
-      transform: translate(100vw, 0);
+  @keyframes active-rotation {
+    0% {
+      transform: rotate(0deg);
     }
-  }
-
-  @include phone {
-    bottom: 26px !important;
-
-    &--left {
-      left: 50%;
-      transform: translate(-50%);
+    100% {
+      transform: rotate(360deg);
     }
   }
 }
