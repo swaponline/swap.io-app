2c3798bf34166913ecfd198a66696a7c60805337
Denis Sannikov <smaylik911@gmail.com>
2021-09-21 18:54:34 +0700

    change scss selector

--- src/components/UI/SwapSkeleton.vue
+++ src/components/UI/SwapSkeleton.vue
@@ -10,34 +10,37 @@ export default {
 
 <style lang="scss">
 .swap-skeleton {
-  &.theme--dark.v-skeleton-loader,
-  &.theme--light.v-skeleton-loader {
-    .v-skeleton-loader__avatar,
-    .v-skeleton-loader__button,
-    .v-skeleton-loader__chip,
-    .v-skeleton-loader__divider,
-    .v-skeleton-loader__heading,
-    .v-skeleton-loader__image,
-    .v-skeleton-loader__text {
-      background: var(--main-button-background-hover);
-    }
-    .v-skeleton-loader__actions,
-    .v-skeleton-loader__article,
-    .v-skeleton-loader__card-heading,
-    .v-skeleton-loader__card-text,
-    .v-skeleton-loader__date-picker,
-    .v-skeleton-loader__list-item,
-    .v-skeleton-loader__list-item-avatar,
-    .v-skeleton-loader__list-item-avatar-three-line,
-    .v-skeleton-loader__list-item-avatar-two-line,
-    .v-skeleton-loader__list-item-text,
-    .v-skeleton-loader__list-item-three-line,
-    .v-skeleton-loader__list-item-two-line,
-    .v-skeleton-loader__table-heading,
-    .v-skeleton-loader__table-tbody,
-    .v-skeleton-loader__table-tfoot,
-    .v-skeleton-loader__table-thead {
-      background: transparent;
+  &.theme--dark,
+  &.theme--light {
+    .v-skeleton-loader {
+      &__avatar,
+      &__button,
+      &__chip,
+      &__divider,
+      &__heading,
+      &__image,
+      &__text {
+        background: var(--main-button-background-hover);
+      }
+
+      &__actions,
+      &__article,
+      &__card-heading,
+      &__card-text,
+      &__date-picker,
+      &__list-item,
+      &__list-item-avatar,
+      &__list-item-avatar-three-line,
+      &__list-item-avatar-two-line,
+      &__list-item-text,
+      &__list-item-three-line,
+      &__list-item-two-line,
+      &__table-heading,
+      &__table-tbody,
+      &__table-tfoot,
+      &__table-thead {
+        background: transparent;
+      }
     }
   }
 }
