c5fc4a1fd99422f761b4d6161621d7cd429f626a
Tatyana <syrtsova.tanya@gmail.com>
2022-01-20 13:54:34 +0700

    fix groupWalletsBy tests

--- tests/unit/utils/wallets.spec.js
+++ tests/unit/utils/wallets.spec.js
@@ -1,23 +1,47 @@
 import { groupWalletsBy } from '@/utils/wallets'
 import { mockWallets, btcGroup, bitcoinRenBTCWallet, bitcoinWBTCWallet } from '../__mocks__/wallets.mock'
 
+const ethGroup = {
+  networkId: 'ethereum',
+  value: 202.2137,
+  wallets: [bitcoinWBTCWallet, bitcoinRenBTCWallet]
+}
+
 describe('groupWalletsBy', () => {
+  const findWallet = (wallet, wallets) => {
+    return wallets.find(({ networkId, address, coin }) => {
+      return wallet.networkId === networkId && wallet.address === address && wallet.coin === coin
+    })
+  }
   it('groups wallets by asset', () => {
     const groupedWallets = groupWalletsBy(mockWallets, 'asset')
+    const btcGroupInGroupedWallets = groupedWallets.find(({ asset }) => asset.name === btcGroup.asset.name)
 
     expect(groupedWallets.length).toEqual(3)
-    expect(groupedWallets[0]).toEqual(btcGroup)
+    expect(btcGroupInGroupedWallets.asset).toEqual(btcGroup.asset)
+    expect(btcGroupInGroupedWallets.value).toBe(btcGroup.value)
+    expect(btcGroupInGroupedWallets.wallets.length).toBe(btcGroup.wallets.length)
+
+    btcGroup.wallets.forEach(wallet => {
+      const walletInGroup = findWallet(wallet, btcGroupInGroupedWallets.wallets)
+
+      expect(walletInGroup).toBeTruthy()
+    })
   })
 
   it('groups wallets by networkId', () => {
     const groupedWallets = groupWalletsBy(btcGroup.wallets, 'networkId')
-    const ethereumGroup = {
-      networkId: 'ethereum',
-      value: 202.2137,
-      wallets: [bitcoinWBTCWallet, bitcoinRenBTCWallet]
-    }
+    const ethGroupInGroupedWallets = groupedWallets.find(({ networkId }) => networkId === ethGroup.networkId)
 
     expect(groupedWallets.length).toEqual(3)
-    expect(groupedWallets[1]).toEqual(ethereumGroup)
+    expect(ethGroupInGroupedWallets.networkId).toBe(ethGroup.networkId)
+    expect(ethGroupInGroupedWallets.value).toBe(ethGroup.value)
+    expect(ethGroupInGroupedWallets.wallets.length).toBe(ethGroup.wallets.length)
+
+    ethGroup.wallets.forEach(wallet => {
+      const walletInGroup = findWallet(wallet, ethGroupInGroupedWallets.wallets)
+
+      expect(walletInGroup).toBeTruthy()
+    })
   })
 })
